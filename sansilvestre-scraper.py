a981d5e7993527af616f1bf04f60b2298f6ecd23
franpelirrojo <franfc.00@gmail.com>
2026-02-05 16:15:29 +0100

    solucion bug de paginación con enlaces de dorsales

--- sansilvestre-scraper.py
+++ sansilvestre-scraper.py
@@ -1,4 +1,5 @@
 import scrapy
+import re
 import sys
 import json
 from scrapy.crawler import CrawlerProcess
@@ -10,8 +11,8 @@ settings_d = {
     "DOWNLOAD_TIMEOUT" : 5,
     "COOKIES_ENABLED" : False,
     'DOWNLOAD_DELAY': 0,
-    'CONCURRENT_ITEMS': 10,
-    'CONCURRENT_REQUESTS': 5,
+    'CONCURRENT_ITEMS': 25,
+    'CONCURRENT_REQUESTS': 12,
     'AUTOTHROTTLE_ENABLED': False
 }
 
@@ -22,43 +23,79 @@ class SanSilvestreSp(scrapy.Spider):
         super(SanSilvestreSp, self).__init__(*args, **kwargs)
         self.allowed_domains = params.get("allowed_domains")
         self.start_urls = params.get("start_urls")
-        self.carrera_link = params.get("carrera_link")
-        self.categoria_link = params.get("categoria_link")
-        self.fields = params.get("fields")
+        self.selectores = params.get("selectores")
 
     def parse(self, response):
-        carreras = response.css(self.carrera_link).getall()
-        yield from response.follow_all(carreras, callback=self.parse_eventos)
+        carreras_url = response.css(self.selectores["carrera_link"]).getall()
+        nombre_carrera = response.css(self.selectores["nombre_carrera"]).getall()
+        año_carrera = response.css(self.selectores["año_carrera"]).getall()
 
-    def parse_eventos(self, response):
-        url = response.url
-        if "competicion" in url:
-            yield response.follow(url, callback=self.parse_resultados)
+        for url, nombre, año in zip(carreras_url, nombre_carrera, año_carrera):
+            yield response.follow(url,
+                                  callback=self.parse_eventos,
+                                  cb_kwargs={"edicion":nombre, "año":año})
+
+    def parse_eventos(self, response, **kwargs):
+        item = kwargs.copy()
+        if "competicion" in (url := response.url):
+            yield response.follow(url,
+                                  callback=self.parse_resultados,
+                                  cb_kwargs=item)
         else:
-            categorias_url = response.css(self.categoria_link).getall()
-            categorias = response.css(self.fields['categoria']).getall()
+            categorias_url = response.css(self.selectores["categoria_link"]).getall()
+            categorias = response.css(self.selectores['categoria']).getall()
             for url, categoria in zip(categorias_url, categorias):
-                yield response.follow(url, 
+                item["categoria"] = categoria.strip()
+                yield response.follow(url,
                                       callback=self.parse_resultados,
-                                      cb_kwargs={"categoria" : categoria})
+                                      cb_kwargs=item)
 
-    def parse_resultados(self, response, categoria): 
-        campos = response.css(self.fields['campos']).getall()
-        tupla = response.css(self.fields['tupla']).getall()
-        pag = response.css(self.fields['pagination']).get()
-        for i in range(0, len(tupla), len(campos)):
-            item = {}
-            for k, campo in enumerate(campos): 
-                item[campo] = tupla[i+k]
-            item['categoria'] = categoria.strip()
-            yield item
+    def parse_resultados(self, response, **kwargs): 
+        corredor_links = response.css(self.selectores['tupla_link']).getall()
+        campos = response.css(self.selectores['tabla_campos']).getall()
+        valores = response.css(self.selectores['tabla_valores']).getall()
+        pag = response.css(self.selectores['pagination']).get()
 
+        filas = [valores[i : i + len(campos)] for i in range(0, len(valores), len(campos))]
+        for i, fila in  enumerate(filas):
+            item = kwargs.copy()
+            for campo, valor in zip(campos, fila):
+                item[campo] = valor
+
+            if corredor_links:
+                yield response.follow(corredor_links[i],callback=self.parse_corredor,cb_kwargs=item)
+            else:
+                yield item
 
         if pag: yield response.follow(pag,
-                                      callback=self.parse_resultados, 
-                                      cb_kwargs={"categoria" : categoria})
-        
-        
+                                      callback=self.parse_resultados,
+                                      cb_kwargs=kwargs.copy())
+
+    def parse_corredor(self, response, **kwargs):
+        club = response.xpath(self.selectores['club']).get()
+        campos = response.css(self.selectores['tabla_campos']).getall()
+        valores = response.css(self.selectores['tabla_valores']).getall()
+        campos = [clean for x in campos if (clean := re.sub(r'\s+', '', x))]
+        tupla = [clean for x in valores if (clean := re.sub(r'\s+', '', x))]
+
+        item = kwargs.copy()
+        if club: item['club'] = club.replace('\n', '')
+
+        controles = []
+        if campos:
+            tuplas = [valores[i : i + len(campos)] for i in range(0, len(valores), len(campos))]
+            for tupla in tuplas:
+                control = {}
+                for campo, valor in zip(campos, tupla): 
+                    control[campo] = valor
+                controles.append(control)
+            item['controles'] = controles
+        else:
+            valores = response.css(self.selectores['virtual_valores']).getall()
+            if valores: controles.append({valores[i] for i in range(0, 2)})
+
+        yield item
+
 
     @classmethod
     def from_crawler(cls, crawler, *args, **kwargs): #TODO:revisar entrada de esto
